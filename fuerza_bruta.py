ab6bb31cb3c2b9ea58bc60c247c9413df3447e8f
AlkimistaRavith <ravith.d3@gmail.com>
2025-05-23 00:39:51 +0000

    ejericcio 3 completo

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -1,18 +1,41 @@
 import getpass
 from string import ascii_lowercase
 
+#En github no se registran simbolos por letras ingresadas en password.
 password = getpass.getpass("Ingrese una contraseña: ")
 password = password.lower()
+password = password.replace(" ", "$")
+
 intentos = 0
+activar = 0
 
-def contar_intentos(password):
-    password = password.lower()
-    intentos = 0
+#Condicional para restringir uso a solo letras
+for letra in password:
+    if letra not in ascii_lowercase:
+        print("La contraseña solo debe contener letras del abecedario (sin ñ, números, símbolos ni espacios). \nPor favor inténtalo nuevamente.")
+        activar = "no"
+        break
+    else:
+        activar = "si"
 
-    for letra in password:
-        for letra_abecedario in ascii_lowercase:
-            intentos += 1
-            if letra == letra_abecedario:
-                break  # Se adivinó la letra, pasar a la siguiente
+if activar == "si" :
+    #función para registrar la cantidad de intentos por iteración
+    def contar_intentos(password):
+        #para que no importe si se escribe con mayuscula o minuscula.
+        password = password.lower()
+        intentos = 0
 
-    return intentos
+        #iteración de cada letra.
+        for letra in password:
+            #comapración y contador de intentos.
+            for letra_abecedario in ascii_lowercase:
+                intentos += 1
+                if letra == letra_abecedario:
+                    break 
+        #regreso a intentos = 0 para volver a contar.
+        return intentos
+
+    #suma de los contar_intentos por cada iteración.
+    total_intentos = contar_intentos(password)
+
+    print(f"La contraseña fue adivinada en {total_intentos} intentos.")
